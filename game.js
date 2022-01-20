ef58c1072fb9d4e755e3d4bd68a96b078f6bbc09
wdattila <wdattila@gmail.com>
2022-01-20 15:35:56 +0100

    add: update death

--- game.js
+++ game.js
@@ -242,20 +242,20 @@ window.addEventListener("keydown", function (event) {
 }, true);
 
 function getTranslateX() {
-
   let frog = document.querySelector('.frog')
     if (frog && frog.parentElement.classList.contains("road")){
-    let actualRow = frog.parentNode.getAttribute("data-row");
-    let cartype = document.querySelector(`.row[data-row="${actualRow}"]`).getAttribute("cartype")
-    let cars = document.querySelectorAll(`.row[data-row="${actualRow}"] .car${cartype}`)
-    for(let car=0; car<cars.length;car++){
-        if(frog.getBoundingClientRect()['x'] < cars[car].getBoundingClientRect()['right']
-            && cars[car].getBoundingClientRect()['right']  < frog.getBoundingClientRect()['right'])
-        {
-            dieAnim();
-            lostLife();
+        let actualRow = frog.parentElement.getAttribute("data-row");
+        let cartype = frog.parentElement.getAttribute("cartype")
+        let cars = document.querySelectorAll(`.row[data-row="${actualRow}"] .car${cartype}`)
+        let left = frog.getBoundingClientRect()['x'];
+        let right = frog.getBoundingClientRect()['right'];
+        for(let car of cars){
+            if(left < car.getBoundingClientRect()['right']
+                && car.getBoundingClientRect()['left']  < right)
+            {
+                roadkill();
+            }
         }
-    }
 }
 }
 function getLife(){
@@ -276,23 +276,22 @@ function lostLife(){
         console.log("dead")
         gameOver()
     }else{
-    setTimeout(respawn,1000)}
+    respawn();}
 }
 
-function dieAnim(){
+function roadkill(){
     let frog = document.querySelector('.frog');
     let dieAnim = document.createElement("div");
-    dieAnim.classList.add('dieAnim');
+    dieAnim.classList.add('roadkill');
     frog.parentNode.appendChild(dieAnim);
-    let style = frog.currentStyle || window.getComputedStyle(frog);
-    const root = document.querySelector(':root');
-    root.style.setProperty("--frog-margin", `${style.marginLeft}`)
-    dieAnim.setAttribute("style", "animation: die-on-road 1000ms steps(4);");
     frog.remove()
+    dieAnim.addEventListener("animationend", event => {
+        event.currentTarget.remove();
+        lostLife();
+        event.stopPropagation();
+    })
 }
 function respawn(){
-    let dieAnim = document.querySelector('.dieAnim');
-    dieAnim.remove()
     const root = document.querySelector(':root');
     root.style.setProperty("--frog-margin", `336px`)
     let newFrog = document.createElement("div");
